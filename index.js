74af43d39c3212ccebcd84979f309b3fe9ca35cb
Jonathan Slominski <jonathan.slominski@monscierge.com>
2016-10-25 15:34:30 -0500

    init

---
+++ index.js
@@ -0,0 +1,138 @@
+/**
+ * Created by jonathanslominski on 10/25/16.
+ */
+const Joi = require('joi');
+const _ = require('lodash');
+
+var exports = module.exports = {};
+
+function translate(schema, object, translator, apiKey, sourceLanguage, targetLanguage, next) {
+
+  Joi.validate(object, schema, function(err, result) {
+
+    if (err === null) {
+
+      var clonedSchema = _.cloneDeep(schema)
+
+      stripSchema(clonedSchema, function(err, hasChildren) {
+
+        Joi.validate(object, clonedSchema, function(err, result) {
+
+          if (err === null) {
+            translateObject(result, translator, apiKey, sourceLanguage, targetLanguage, function (err, obj) {
+              object = _.merge(object, obj);
+              return next(err, object);
+            });
+          } else {
+            return next(err, object);
+          }
+
+        });
+
+      });
+
+    } else {
+      return next(err, object);
+    }
+
+  });
+
+}
+
+function stripSchema(schema, next) {
+
+  var hasLocalizedChildren = false;
+
+  _.each(schema._inner.children, function(childProperty) {
+
+    if (_.indexOf(childProperty.schema._tags, 'localizedString') > -1  && _.difference(_.map(childProperty.schema._inner.children, 'key'), ['translate', 'is_machine_translated', 'value', 'is_dirty']).length == 0) {
+      console.log(`keep ${childProperty.key}!`)
+      hasLocalizedChildren = true;
+    } else {
+
+      stripSchema(childProperty.schema, function(err, hasChildren) {
+
+        if (hasChildren === true) {
+          console.log(`keep ${childProperty.key}!`)
+          hasLocalizedChildren = true;
+        } else {
+          console.log(`strip ${childProperty.key}!`)
+          childProperty['schema']['_flags']['strip'] = true;
+        }
+      });
+    }
+
+  });
+
+  return next(null, hasLocalizedChildren);
+}
+
+function translateObject(obj, translator, apiKey, sourceLanguage, targetLanguage, next) {
+
+  var requests = _.keys(obj).map((k) => {
+
+    return new Promise((resolve) => {
+
+      if (_.indexOf(_.keys(obj[k]), 'is_dirty') > -1 && _.indexOf(_.keys(obj[k]), 'value') > -1 && _.indexOf(_.keys(obj[k]), 'is_machine_translated') > -1 && _.indexOf(_.keys(obj[k]), 'translate') > -1) {
+
+        if (obj[k].translate === true && obj[k].is_machine_translated === true) {
+
+          if (translator.toLowerCase() === 'google') {
+
+            var googleTranslate = require('google-translate')(apiKey);
+
+            if (!sourceLanguage || sourceLanguage === null) {
+
+              googleTranslate.translate(obj[k].value, targetLanguage, function (err, translation) {
+
+                if (err === null) {
+                  obj[k].value = translation.translatedText;
+                }
+
+                resolve();
+              });
+
+            } else {
+
+              googleTranslate.translate(obj[k].value, sourceLanguage, targetLanguage, function (err, translation) {
+
+                if (err === null) {
+                  obj[k].value = translation.translatedText;
+                }
+
+                resolve();
+              });
+
+            }
+
+          } else {
+            resolve();
+          }
+
+        } else {
+          resolve();
+        }
+
+      } else {
+        translateObject(obj[k], translator, apiKey, sourceLanguage, targetLanguage, function(err, result) {
+
+          obj[k] = result;
+          resolve();
+
+        });
+
+      }
+
+    });
+
+  });
+
+  Promise.all(requests).then(function () {
+
+    return next(null, obj);
+
+  });
+
+}
+
+exports.translate = translate;
---
+++ test/test.js
@@ -0,0 +1,85 @@
+/**
+ * Created by jonathanslominski on 10/25/16.
+ */
+const localize = require('../index');
+const Joi = require('joi');
+const googleApiKey = 'AIzaSyAvHprYyzBs6KoJ0XAAmpWEnsEfi2RfRDA';
+
+var schema = Joi.object().keys({
+   ref_id: Joi.string().guid(),
+   parents: Joi.object().pattern(/place|feature/, Joi.array().items(Joi.string().guid())).meta({ className: "ParentIdModels"}),
+   menu: Joi.object().keys({
+       name: Joi.object().keys({ translate: Joi.boolean().required(), is_machine_translated: Joi.boolean().required(), is_dirty: Joi.boolean().required(), value: Joi.string().required() }).tags(['localizedString']).required(),
+       description: Joi.object().keys({ translate: Joi.boolean().required(), is_machine_translated: Joi.boolean().required(), is_dirty: Joi.boolean().required(), value: Joi.string().required() }).tags(['localizedString']),
+       is_active: Joi.boolean(),
+       welcome_message: Joi.object().keys({ translate: Joi.boolean().required(), is_machine_translated: Joi.boolean().required(), is_dirty: Joi.boolean().required(), value: Joi.string().required() }).tags(['localizedString']),
+       fk_request_template: Joi.string().guid(),
+       fk_logo: Joi.string().guid(),
+       images: Joi.array().items(Joi.string().guid()),
+       gratuity: Joi.object().keys({
+           percent: Joi.number().precision(2).min(0.00).max(100.00),
+           partyMin: Joi.number().min(0)
+       }).meta({ className: "GratuityModel"}),
+       localization: Joi.object().keys({
+         language: Joi.object().keys({ translate: Joi.boolean().required(), is_machine_translated: Joi.boolean().required(), is_dirty: Joi.boolean().required(), value: Joi.string().required() }).tags(['localizedString']),
+         locale: Joi.object().keys({ translate: Joi.boolean().required(), is_machine_translated: Joi.boolean().required(), is_dirty: Joi.boolean().required(), value: Joi.string().required() }).tags(['localizedString'])
+       }).meta({ className: "LocalizationModel"}),
+       nested: Joi.object().keys({
+         nested: Joi.object().keys({
+         nested_language: Joi.object().keys({ translate: Joi.boolean().required(), is_machine_translated: Joi.boolean().required(), is_dirty: Joi.boolean().required(), value: Joi.string().required() }).tags(['localizedString']),
+         nested_locale: Joi.object().keys({ translate: Joi.boolean().required(), is_machine_translated: Joi.boolean().required(), is_dirty: Joi.boolean().required(), value: Joi.string().required() }).tags(['localizedString'])
+        })
+       }),
+       ordinal: Joi.number().integer().min(0),
+   }).required().meta({ className: "MenuModel"}),
+   created_on: Joi.date().timestamp(),
+   updated_on: Joi.date().timestamp(),
+   links: Joi.object().meta({ className: "LinksModel"})
+}).meta({ className: "MenuRecord"});
+
+var testModel = {
+    ref_id: '97170e81-8b55-4cad-8748-5a94658b91d6',
+    parents: {
+      "feature": [
+        'e91563c3-e052-45f0-8971-5ea27f51cb7e',
+        '97170e81-8b55-4cad-8748-5a94658b91d6'
+      ],
+      "place": [
+        'e91563c3-e052-45f0-8971-5ea27f51cb7e',
+        '97170e81-8b55-4cad-8748-5a94658b91d6'
+      ]
+    },
+    menu: {
+      name: { translate: false, value: 'Test Menu Object', is_dirty: false, is_machine_translated: true },
+      description: { translate: true, value: 'This is a description of the Test Menu Object', is_dirty: false, is_machine_translated: true },
+      is_active: true,
+      welcome_message: { translate: true, value: 'Welcome to the Test Menu Object', is_dirty: false, is_machine_translated: true },
+      fk_request_template: '720cfd35-7d11-4aed-9404-2f84beab38fc',
+      fk_logo: '1892d407-28db-4c91-ad5b-bd2c596a201d',
+      images: ['1892d407-28db-4c91-ad5b-bd2c596a201d',
+        'edc9593f-6441-4e0d-94c3-c1b4ccd7d25d',
+        'cdcd0518-86ba-45e8-a7d2-675867cab29a'],
+      gratuity: {
+        percent: 18,
+        partyMin: 6
+      },
+      localization: {
+        language: { translate: true, value: 'hello', is_dirty: false, is_machine_translated: true },
+        locale: { translate: true, value: 'goodbye', is_dirty: false, is_machine_translated: true }
+      },
+      nested: {
+        nested: {
+          nested_language: { translate: true, value: 'hello', is_dirty: false, is_machine_translated: true },
+          nested_locale: { translate: true, value: 'goodbye', is_dirty: false, is_machine_translated: true }
+        }
+      },
+      ordinal: 0
+    },
+    created_on: new Date().getTime(),
+    updated_on: new Date().getTime()
+  };
+
+// translate the object using supplied schema
+localize.translate(Joi.reach(schema, 'menu'), testModel.menu, 'google', googleApiKey, 'en', 'es', (err, result) => {
+  console.log(testModel);
+});
